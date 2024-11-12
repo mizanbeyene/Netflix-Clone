185fc48a73774afe4cd7deb7ee5b82c00d0794e2
mizanbeyene <mizanbeyene4@gmail.com>
2024-11-12 11:25:11 -0500

    Netflix-Clone git commit -m Netflix-Clone git commit -m Netflix-Clone

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  
-
   return (
-    <>
      <div className='App'>
-      <h1>Hello World</h1>
-
+      <Home/>
      </div>
-    </>
   )
 }
 
