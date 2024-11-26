b5c196db827047d3dcd38a414fbad0baa2353dd8
mizanbeyene <mizanbeyene4@gmail.com>
2024-11-26 11:40:59 -0500

    netflix-deploy

--- Netflix-Clone/vite.config.js
+++ Netflix-Clone/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  badse:"/Netflix-Clone/",
+  base:"/Netflix-Clone/",
   plugins: [react()],
 })
